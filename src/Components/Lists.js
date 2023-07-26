a4dd385539439fbbe825e789589cda7c9e43a3b6
WebworldContent <jeomarkoo1@gmail.com>
2023-07-26 22:01:29 +0530

    Add: Adding beging react structre code and MUI support

---
+++ src/Components/Lists.js
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+
+export default function CheckboxList() {
+  const [checked, setChecked] = React.useState([0]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return (
+    <List sx={{ width: '100%', maxWidth: 560, bgcolor: 'background.paper' }}>
+      {[0, 1, 2, 3].map((value) => {
+        const labelId = `checkbox-list-label-${value}`;
+
+        return (
+          <React.Fragment>
+          <ListItem
+            key={value}
+            secondaryAction={
+              <IconButton edge="end" aria-label="comments">
+                <Button variant="contained" href="#contained-buttons">
+                  Link
+                </Button>
+              </IconButton>
+            }
+            disablePadding
+          >
+            
+            <ListItemButton role={undefined} onClick={handleToggle(value)}>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={checked.indexOf(value) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+            </ListItemButton>
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          </React.Fragment>
+        );
+      })}
+    </List>
+  );
+}

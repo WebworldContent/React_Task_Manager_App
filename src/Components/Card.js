883f69fdf74f2c735980ad979615fad639476043
WebworldContent <jeomarkoo1@gmail.com>
2023-08-30 01:25:53 +0530

    updating the design for drag n drop functionality

---
+++ src/Components/Card.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {Card, CardActions, CardContent, Button, Typography} from '@mui/material';
+
+export default function CardElement() {
+  return (
+    <Card sx={{ minWidth: 275, margin: 1 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,12 @@
 import React, { Fragment, useState } from "react";
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import TaskStatus from "./TaskStatus";
 import TaskCategory from './TaskCategory';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import {Stack, Button, Box, Grid} from '@mui/material';
 import List from './Lists';
 import { useNavigate } from "react-router-dom";
 import { MenuHeader } from "./Header";
 import { getAuth } from "firebase/auth";
+import CardElement from "./Card";
 
 export const Home = () => {
   const [searchedStatus, setSearchedStatus] = useState('');
@@ -24,36 +22,29 @@ export const Home = () => {
     return (
     <Fragment>
       <MenuHeader auth={auth} />
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}>
         <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2}>
-            <Grid item md={2.5} xs={2} sm={2}></Grid>
-              <Grid item md={8} xs={8} sm={8}>
-                <img
-                  src={`https://wallpaperaccess.com/full/1489353.jpg`}
-                  srcSet={`https://wallpaperaccess.com/full/1489353.jpg`}
-                  alt={'task list img'}
-                  loading="lazy"
-                  style={{width: '955px', height: '500px'}}
-                />
+          <Grid container spacing={2} justifyContent="center" alignItems="center">
+              <Grid item md={8} xs={10} sm={10}>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                  <CardElement/><CardElement/><CardElement/><CardElement/>
+                </div>
               </Grid>
-            <Grid item md={2.5} xs={4} sm={4}></Grid>
-            <Grid item md={8} xs={8} sm={8}>
-              <div style={{ marginTop: '40px' }}>
-                <Stack
-                  direction={{ xs: 'column', sm: 'column', md: 'row' }}
-                  spacing={{ xs: 2, sm: 10, md: 23 }}
-                >
-                  <TaskStatus taskStatus={getTaskStatus} />
-                  <Button variant="contained" onClick={() => navigate('add-task')}>Add Tasks</Button>
-                  <TaskCategory taskCategory={getTaskCategory} />
-                </Stack>
-              </div>
-              <div style={{ marginTop: '100px' }}>
-                <List searchedStatus={searchedStatus} taskCategory={taskCategory} auth={auth} />
-              </div>
-            </Grid>
-            <Grid item md={2.5} xs={2} sm={2}></Grid>
+              <Grid item md={8} xs={10} sm={10}>
+                <div style={{ marginTop: '40px' }}>
+                  <Stack
+                    direction={{ xs: 'column', sm: 'column', md: 'row' }}
+                    spacing={{ xs: 2, sm: 10, md: 45 }}
+                  >
+                    <TaskStatus taskStatus={getTaskStatus} />
+                    <Button variant="contained" onClick={() => navigate('add-task')}>Add Tasks</Button>
+                    <TaskCategory taskCategory={getTaskCategory} />
+                  </Stack>
+                </div>
+                <div style={{ marginTop: '100px', display: 'flex', justifyContent: 'center' }}>
+                  <List searchedStatus={searchedStatus} taskCategory={taskCategory} auth={auth} />
+                </div>
+              </Grid>
           </Grid>
         </Box>
       </div>
